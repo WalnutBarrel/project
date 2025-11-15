2ba360f8b4fc086a4733e95c9a810760e12ca713
WalnutBarrel <87712034+WalnutBarrel@users.noreply.github.com>
2025-11-15 07:29:23 +0530

    ...

--- backend/pro1/api/models.py
+++ backend/pro1/api/models.py
@@ -9,3 +9,13 @@ class User(models.Model):
 
     def __str__(self):
         return self.email
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=150)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    description = models.TextField()
+    image = models.URLField()  # image link (or later use ImageField)
+
+    def __str__(self):
+        return self.title

bdabf6d3e28bdad66846c42da2dc3feffcf001ce
meghraj <87712034+WalnutBarrel@users.noreply.github.com>
2025-11-19 08:46:30 +0530

    backup till19-11

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -13,7 +13,7 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/project">
       <App />
     </BrowserRouter>
   </StrictMode>

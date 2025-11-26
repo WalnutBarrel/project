96c944b927ee7d067669cc8cd849744b11e3d0d0
Krunal1712 <87712034+WalnutBarrel@users.noreply.github.com>
2025-11-26 08:33:09 +0530

    Configured project for GitHub Pages deployment

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,6 @@ import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
@@ -13,7 +12,7 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/project">
+    <BrowserRouter basename={import.meta.env.MODE === "production" ? "/project" : "/"}>
       <App />
     </BrowserRouter>
   </StrictMode>
